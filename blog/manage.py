301fd940c7bbb358b2a2cda7a2825799f145d943
Tao Wu <bjwt@linux.vnet.ibm.com>
2015-08-31 16:00:44 +0800

    deployment

--- blog/manage.py
+++ blog/manage.py
@@ -3,6 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
+    for arg in sys.argv:
+        print arg
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings")
 
     from django.core.management import execute_from_command_line
